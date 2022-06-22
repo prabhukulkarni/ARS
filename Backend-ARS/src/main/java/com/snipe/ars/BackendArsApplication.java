a07a6d22c4572a59e2e05bdf5513a8e6fdde57fc
DESKTOP-FOA72HJ\admin <prabhukulkarni5@gmail.com>
2022-06-22 09:57:30 +0530

    from clone

---
+++ Backend-ARS/src/main/java/com/snipe/ars/BackendArsApplication.java
@@ -0,0 +1,13 @@
+package com.snipe.ars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendArsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendArsApplication.class, args);
+	}
+
+}
---
+++ Backend-ARS/src/test/java/com/snipe/ars/BackendArsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snipe.ars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendArsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
